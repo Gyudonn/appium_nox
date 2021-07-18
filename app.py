dece3ad9641fe34613e2efd0ee1ba944bb920a75
Gyudonn <xjirmpmxjirmpm@gmail.com>
2021-07-19 03:02:26 +0900

    update

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from appium import webdriver
 import aircv as ac
+import time
+import os
 
-imsrc = None
-imobj = None
+
+imsrc_name = None
+imobj_name = None
+img_location = [['confirm', 'confirm_button'], ['ani', 'ani_skip'], ['resoult', 'resoult_button']]
 
 desired_caps = {}
 desired_caps['platformName'] = "Android"         # 声明是ios还是Android系统
@@ -15,54 +19,93 @@ desired_caps['newCommandTimeout'] = '10000'
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)          # 建立 session
 
+def set_img_name(img_name):
+	img_fullname = img_name + '.png'
+	return img_fullname
+
 def get_size():
 	phone_screen_size_width = driver.get_window_size()['width']
 	phone_screen_size_height = driver.get_window_size()['height']
-
 	return(phone_screen_size_width, phone_screen_size_height)
 
-def matchImg(imgsrc,imgobj,confidence):#imgsrc=原始圖像，imgobj=待查找的圖片
-    imsrc = ac.imread('oring.png')
-    imobj = ac.imread('icon.png')
+def screen_shot():
+	i = 0
+	save_file = False
+	while save_file != True:
+		filename = ('screen_shot' + str(i) + '.png')
+		print(filename)
+		if os.path.isfile(filename):
+			print('檔案存在。')
+			i = i + 1
+		else:
+			print('檔案不存在。')
+			driver.get_screenshot_as_file(filename)#截屏手機
+			save_file = True
+
+def matchImg(confidence):#imgsrc=原始圖像，imgobj=待查找的圖片
+    imsrc = ac.imread(imsrc_name)
+    imobj = ac.imread(imobj_name)
  
-    match_result = ac.find_template(imsrc,imobj,confidence)
+    match_result = ac.find_template(imsrc, imobj, confidence)
     if match_result is not None:
         match_result['shape']=(imsrc.shape[1],imsrc.shape[0])#0為高，1為寬
 
     return match_result
 
 def tab_image():
-	photo_position = driver.get_screenshot_as_file('oring.png')#截屏手機
 	confidencevalue = 0.8  # 定義相似度
-	position = matchImg(imsrc, imobj, confidencevalue)# 用第一步的方法，實際就是find_template()方法
-	print(matchImg(imsrc, imobj, confidencevalue))
+	position = matchImg(confidencevalue)# 用第一步的方法，實際就是find_template()方法
 
 	if position != None:
 		x, y = position['result']
 		driver.tap([(x, y)])
 
-
 def swip_right(t):
-	l=get_size()
-	x1=int(l[0]*0.05)
-	y1=int(l[1]*0.5)
-	x2=int(l[0]*0.75)
+	screen_size = get_size()
+	x1 = int(screen_size[0] * 0.05)
+	y1 = int(screen_size[1] * 0.5)
+	x2 = int(screen_size[0] * 0.75)
 	driver.swipe(x1, y1, x2, y1, t)
 
 while True:
-	mode = input('Enter operator: ')
-	if mode == 't':
+	mode = input('Enter operator(tap, swip, shot, q): ')
+	if mode == 'tap':
 		#driver.find_elements_by_id('com.userjoy.sin_mcard:id/view_platform_item_v')[0].click()
 		tab_image()
+	if mode == 'match':
+		print(matchImg(0.5))
+		tab_image()
+	if mode == 'shot':
+		screen_shot()
+	if mode == 'swip':
+		swip_right(100)
+	if mode == 'auto':
+		while True:
+			time.sleep(2)
+			imsrc_name = set_img_name(img_location[0][0])
+			imobj_name = set_img_name(img_location[0][1])
+			tab_image()
+
+			time.sleep(3)
+			swip_right(100)
+
+			time.sleep(1)
+			imsrc_name = set_img_name(img_location[1][0])
+			imobj_name = set_img_name(img_location[1][1])
+			tab_image()
+
+			time.sleep(1)
+			tab_image()
+
+			time.sleep(2)
+			imsrc_name = set_img_name(img_location[2][0])
+			imobj_name = set_img_name(img_location[2][1])
+			tab_image()
 	if mode == 'q':
 		print('Exit')
 		driver.quit()      # 退出 session
-		continue
-	if mode == 's':
-		swip_right(100)
-	if mode == 'unity':
-		driver.find_elements_by_class_name("android.view.View")[0]
-
+		time.sleep(5)
+		break
 
 # def main():
 # 	tab_image()
